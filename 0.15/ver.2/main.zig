0d9e1f8d081432bff226ad0df216472555fd52ce
TongaEast <166471914+Ru7139@users.noreply.github.com>
2025-12-02 20:36:03 +0800

    Update main.zig

--- 0.15/ver.2/main.zig
+++ 0.15/ver.2/main.zig
@@ -1,5 +1,19 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // const cpu_cores = std.os
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
+    const allocator = gpa.allocator();
+
+    var process = std.process.Child.init(&[_][]const u8{ "adb", "-s", "16384", "exec-out", "screencap" }, allocator);
+
+    process.stdout_behavior = .Pipe;
+    process.stdout_behavior = .Pipe;
+
+    try process.spawn();
+
+    const stdout = process.stdout.?;
+    // const reader = stdout.reader();
+
+    std.debug.print("{}", @sizeOf(stdout));
 }
