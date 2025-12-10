e9b174812d6b0ad23da4269a08642c7afd98dc18
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-12-10 09:43:34 +0900

    moved

--- 0.15/ver.1/test/main.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-
-const expect = std.testing.expect;
-const assert = std.debug.assert;
-const print = std.debug.print;
-const mem_euqal = std.mem.eql;
-
-pub fn main() !void {}
--- 0.15/ver.1/test/zh/test_zh_doc_3.zig
+++
@@ -1,7 +0,0 @@
-test "3.1 crates management" {}
-
-test "3.2 build system" {}
-
-test "3.3 unit testing" {}
-
-test "3.4 style" {}
