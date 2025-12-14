2b41009c33b50d7472c6e5811a106de64539793c
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-12-14 21:17:59 +0800

    Create random_xsr256.zig

---
+++ widgets/random_xsr256.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+
+const n_error = error{ ZeroArrayLenth, ToDo };
+
+pub fn xsr256_with_time_seed() std.Random.Xoshiro256 {
+    const time_seed: u64 = @as(u64, @intCast(std.time.timestamp())) * 2;
+    const xsr256 = std.Random.Xoshiro256.init(time_seed);
+    return xsr256;
+}
+
+pub fn generate_random_array(comptime T: type, allocator: std.mem.Allocator, array_len: usize, xsr: *std.Random.Xoshiro256) ![]T {
+    if (array_len == 0) return error.ZeroArrayLenth;
+    if (T == @Type(u64)) {
+        var arr = try allocator.alloc(u64, array_len);
+        for (arr[0..array_len]) |*i| {
+            i.* = xsr.next();
+        }
+    } else if (T == @Type(f64)) {
+        var arr = try allocator.alloc(f64, array_len);
+        const base = @as(f64, @floatFromInt(2 << 64 - 1));
+        for (arr[0..array_len]) |*i| {
+            i.* = @as(f64, @floatFromInt(xsr.next())) / base;
+        }
+    } else {
+        return error.ToDo;
+    }
+}
+
+test "xsr256_test" {
+    var xsr = xsr256_with_time_seed();
+
+    const gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+    const arr = try generate_random_array(f64, allocator, 100, &xsr);
+    std.debug.print("{}", .{arr});
+}
