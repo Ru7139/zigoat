0f6d7fc6fe195a30074a523b77967a775e2e72e0
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-12-16 22:50:46 +0800

    add odd(u1~u127) test
    xsr256_unsigned_test ---> Success ---> 85ms xsr256_unsigned_test --->
    Success ---> 61ms xsr256_unsigned_test ---> Success ---> 61ms
    xsr256_unsigned_test ---> Success ---> 134ms xsr256_unsigned_test --->
    Success ---> 133ms xsr256_unsigned_test ---> Success ---> 133ms
    xsr256_unsigned_test ---> Success ---> 623ms xsr256_unsigned_test --->
    Success ---> 622ms xsr256_unsigned_test ---> Success ---> 622ms
    xsr256_unsigned_test ---> Success ---> 735ms xsr256_unsigned_test --->
    Success ---> 735ms xsr256_unsigned_test ---> Success ---> 734ms
    xsr256_unsigned_test ---> Success ---> 62ms xsr256_unsigned_test --->
    Success ---> 62ms xsr256_unsigned_test ---> Success ---> 62ms
    xsr256_unsigned_test ---> Success ---> 133ms xsr256_unsigned_test --->
    Success ---> 134ms xsr256_unsigned_test ---> Success ---> 132ms
    xsr256_unsigned_test ---> Success ---> 619ms xsr256_unsigned_test --->
    Success ---> 620ms xsr256_unsigned_test ---> Success ---> 620ms
    xsr256_unsigned_test ---> Success ---> 738ms xsr256_unsigned_test --->
    Success ---> 737ms xsr256_unsigned_test ---> Success ---> 737ms

--- widgets/random_xsr256.zig
+++ widgets/random_xsr256.zig
@@ -51,15 +51,20 @@ test "xsr256_unsigned_test" {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const type_arr1 = [16]type{ u2, u4, u6, u8, u10, u12, u14, u16, u18, u20, u22, u24, u26, u28, u30, u32 };
-    const type_arr2 = [16]type{ u34, u36, u38, u40, u42, u44, u46, u48, u50, u52, u54, u56, u58, u60, u62, u64 };
-    const type_arr3 = [16]type{ u66, u68, u70, u72, u74, u76, u78, u80, u82, u84, u86, u88, u90, u92, u94, u96 };
-    const type_arr4 = [16]type{ u98, u100, u102, u104, u106, u108, u110, u112, u114, u116, u118, u120, u122, u124, u126, u128 };
+    const odd_arr1 = [16]type{ u1, u3, u5, u7, u9, u11, u13, u15, u17, u19, u21, u23, u25, u27, u29, u31 };
+    const odd_arr2 = [16]type{ u33, u35, u37, u39, u41, u43, u45, u47, u49, u51, u53, u55, u57, u59, u61, u63 };
+    const odd_arr3 = [16]type{ u65, u67, u69, u71, u73, u75, u77, u79, u81, u83, u85, u87, u89, u91, u93, u95 };
+    const odd_arr4 = [16]type{ u97, u99, u101, u103, u105, u107, u109, u111, u113, u115, u117, u119, u121, u123, u125, u127 };
 
-    const types_hold_arr = [_]*const [16]type{ &type_arr1, &type_arr2, &type_arr3, &type_arr4 };
+    const even_arr1 = [16]type{ u2, u4, u6, u8, u10, u12, u14, u16, u18, u20, u22, u24, u26, u28, u30, u32 };
+    const even_arr2 = [16]type{ u34, u36, u38, u40, u42, u44, u46, u48, u50, u52, u54, u56, u58, u60, u62, u64 };
+    const even_arr3 = [16]type{ u66, u68, u70, u72, u74, u76, u78, u80, u82, u84, u86, u88, u90, u92, u94, u96 };
+    const even_arr4 = [16]type{ u98, u100, u102, u104, u106, u108, u110, u112, u114, u116, u118, u120, u122, u124, u126, u128 };
+
+    const types_hold_arr = [_]*const [16]type{ &odd_arr1, &odd_arr2, &odd_arr3, &odd_arr4, &even_arr1, &even_arr2, &even_arr3, &even_arr4 };
 
     inline for (types_hold_arr) |types_arr| {
-        inline for (0..5) |_| {
+        inline for (0..3) |_| {
             const time = std.time.milliTimestamp();
             defer std.debug.print("xsr256_unsigned_test ---> Success ---> {}ms\n", .{std.time.milliTimestamp() - time});
 
