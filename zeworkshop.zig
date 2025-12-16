4629d2f71800f62b40e5fe006bdbfddc20bfd068
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-12-16 22:50:11 +0800

    Update zeworkshop.zig

--- zeworkshop.zig
+++ zeworkshop.zig
@@ -1,8 +1,5 @@
 const std = @import("std");
 
 pub fn main() !void {
-    for (0..128) |i| {
-        const index = i + 1;
-        std.debug.print("u{},", .{index * 4});
-    }
+    // const type_a = std.builtin.Type.Vector.
 }
