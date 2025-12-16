9fc075e7a3d0f302ba089a91777ec8807b33b1b0
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-12-17 00:28:05 +0800

    add the test of signed int
    xsr256_signed_test ---> Success ---> 317ms xsr256_signed_test --->
    Success ---> 315ms xsr256_signed_test ---> Success ---> 315ms
    xsr256_signed_test ---> Success ---> 314ms xsr256_signed_test --->
    Success ---> 316ms xsr256_signed_test ---> Success ---> 443ms
    xsr256_signed_test ---> Success ---> 446ms xsr256_signed_test --->
    Success ---> 453ms xsr256_signed_test ---> Success ---> 453ms
    xsr256_signed_test ---> Success ---> 458ms xsr256_signed_test --->
    Success ---> 637ms xsr256_signed_test ---> Success ---> 631ms
    xsr256_signed_test ---> Success ---> 630ms xsr256_signed_test --->
    Success ---> 622ms xsr256_signed_test ---> Success ---> 621ms
    xsr256_signed_test ---> Success ---> 735ms xsr256_signed_test --->
    Success ---> 739ms xsr256_signed_test ---> Success ---> 736ms
    xsr256_signed_test ---> Success ---> 736ms xsr256_signed_test --->
    Success ---> 735ms xsr256_signed_test ---> Success ---> 315ms
    xsr256_signed_test ---> Success ---> 315ms xsr256_signed_test --->
    Success ---> 316ms xsr256_signed_test ---> Success ---> 315ms
    xsr256_signed_test ---> Success ---> 314ms xsr256_signed_test --->
    Success ---> 444ms xsr256_signed_test ---> Success ---> 443ms
    xsr256_signed_test ---> Success ---> 445ms xsr256_signed_test --->
    Success ---> 445ms xsr256_signed_test ---> Success ---> 443ms
    xsr256_signed_test ---> Success ---> 619ms xsr256_signed_test --->
    Success ---> 617ms xsr256_signed_test ---> Success ---> 619ms
    xsr256_signed_test ---> Success ---> 625ms xsr256_signed_test --->
    Success ---> 629ms xsr256_signed_test ---> Success ---> 747ms
    xsr256_signed_test ---> Success ---> 742ms xsr256_signed_test --->
    Success ---> 746ms xsr256_signed_test ---> Success ---> 747ms
    xsr256_signed_test ---> Success ---> 745ms

--- widgets/random_xsr256.zig
+++ widgets/random_xsr256.zig
@@ -79,3 +79,35 @@ test "xsr256_unsigned_test" {
         }
     }
 }
+
+test "xsr256_signed_int_test" {
+    var xsr = xsr256_with_time_seed();
+
+    var gpa = std.heap.DebugAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const odd_arr1 = [16]type{ i1, i3, i5, i7, i9, i11, i13, i15, i17, i19, i21, i23, i25, i27, i29, i31 };
+    const odd_arr2 = [16]type{ i33, i35, i37, i39, i41, i43, i45, i47, i49, i51, i53, i55, i57, i59, i61, i63 };
+    const odd_arr3 = [16]type{ i65, i67, i69, i71, i73, i75, i77, i79, i81, i83, i85, i87, i89, i91, i93, i95 };
+    const odd_arr4 = [16]type{ i97, i99, i101, i103, i105, i107, i109, i111, i113, i115, i117, i119, i121, i123, i125, i127 };
+
+    const even_arr1 = [16]type{ i2, i4, i6, i8, i10, i12, i14, i16, i18, i20, i22, i24, i26, i28, i30, i32 };
+    const even_arr2 = [16]type{ i34, i36, i38, i40, i42, i44, i46, i48, i50, i52, i54, i56, i58, i60, i62, i64 };
+    const even_arr3 = [16]type{ i66, i68, i70, i72, i74, i76, i78, i80, i82, i84, i86, i88, i90, i92, i94, i96 };
+    const even_arr4 = [16]type{ i98, i100, i102, i104, i106, i108, i110, i112, i114, i116, i118, i120, i122, i124, i126, i128 };
+
+    const types_hold_arr = [_]*const [16]type{ &odd_arr1, &odd_arr2, &odd_arr3, &odd_arr4, &even_arr1, &even_arr2, &even_arr3, &even_arr4 };
+
+    inline for (types_hold_arr) |types_arr| {
+        inline for (0..5) |_| {
+            const time = std.time.milliTimestamp();
+            defer std.debug.print("xsr256_signed_test ---> Success ---> {}ms\n", .{std.time.milliTimestamp() - time});
+
+            inline for (types_arr) |T| {
+                const arr = try generate_std_int_array(T, allocator, 1_000_000, &xsr);
+                defer allocator.free(arr);
+            }
+        }
+    }
+}
