e566ac800b2b5520fe72ff3a9721bf0f008ce41b
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-12-16 21:08:09 +0800

    generate u8~u128 and i8~i128

--- zeworkshop.zig
+++ zeworkshop.zig
@@ -1,3 +1,8 @@
 const std = @import("std");
 
-pub fn main() !void {}
+pub fn main() !void {
+    for (0..16) |i| {
+        const index = i + 1;
+        std.debug.print("i{},", .{index * 8});
+    }
+}
